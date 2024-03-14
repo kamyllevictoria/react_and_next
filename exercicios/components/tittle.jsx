9b3447ad9536227b5c1a242aa51d233c5571c155
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-03-14 09:45:42 -0300

    components props

--- exercicios/components/tittle.jsx
+++ exercicios/components/tittle.jsx
@@ -1,8 +1,19 @@
-export default function Tittle(){
-   return(
-        <>
-            <h1>Tittle</h1>
-            <h2>Subtitle</h2>
-        </>
-   )
+export default function Tittle(props){
+    console.log(props)
+    if(props.small){
+        return(
+            <>
+                <p>{props.principal}</p>
+                <p>{props.second}</p>
+            </>
+        )
+    } else{
+        return(
+            <>
+                <h1>{props.principal}</h1>
+                <h2>{props.second}</h2>
+            </>
+        )
+    }
+    
 }
--- exercicios/src/pages/basics/usingTittle.jsx
+++ exercicios/src/pages/basics/usingTittle.jsx
@@ -3,9 +3,15 @@ import Tittle from '../../../components/tittle'
 export default function usingTittle(){
     return(
          <>
-            <Tittle></Tittle>
-            <Tittle></Tittle>
-            <Tittle></Tittle>
+            <Tittle 
+            principal = "Login page" 
+            second = "Add, remove and change things">
+            </Tittle>
+            <Tittle 
+            principal = "Info page" 
+            second = "Enter your email and password"
+            small = {true}>
+            </Tittle>
          </>
     )
  }
