33db4e8b744275e8958a6e8aceb3a6d8502c9d4e
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-03-18 21:42:01 -0300

    css integration part I

---
+++ exercicios/src/pages/css/integration1.jsx
@@ -0,0 +1,10 @@
+
+export default function integration(){
+    return(
+        <>
+            <div className="red">Text one</div>
+            <div className="blue">Text two</div>
+            <div className="pink">Text three</div>
+        </>
+    )
+}
